4b31205240b7999c32d52279234de72a29684cfe
Wellici Araujo <wja1@ifal.edu.br>
2019-09-06 23:58:39 -0300

    header regulado

--- src/pages/PaginaEvento/index.js
+++ src/pages/PaginaEvento/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
-import './index.css';
-import HeaderNav from '../../components/HeaderNav';
-import Rodape from '../../components/Rodape'; 
+import "./index.css";
+import HeaderNav from "../../components/HeaderNav";
+import Rodape from "../../components/Rodape";
 // fetch('http://localhost:3001/eventos', {
 //   method: 'POST',
 //   headers: {
@@ -10,7 +10,7 @@ import Rodape from '../../components/Rodape';
 //   },
 //   body: JSON.stringify()
 // })
-export default function({ match }) {
+export default function({ match, descricao }) {
   const id = match.params.id;
   const [evento, setEvento] = useState({});
 
@@ -19,27 +19,31 @@ export default function({ match }) {
       .then(res => res.json())
       .then(res => {
         setEvento(res);
-        console.log(res)
-      })
-      
-  }, []);
-
+        console.log(res);
+      });
+  }, [id]);
 
   return (
     <>
-      <HeaderNav input={true}/>
-        <div className="container">
+      <HeaderNav input={true} />
+      <div className="container">
         <div class="card mb-7" id="card-img">
-            <img src={evento.banner} class="card-img-top" alt="..."/>
-            <div class="card-body">
-              
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-            </div>
+          <img src={evento.banner} class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </p>
+            <p class="card-text">
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </p>
           </div>
         </div>
-      <Rodape/>
+        <div id="card-desc">{descricao}</div>
+      </div>
+      <Rodape />
     </>
   );
 }
